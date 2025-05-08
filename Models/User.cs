a539c9ccb2c3c56a6568f1c0f54b0d3fb22ecc2d
Mohamed Khaled Gamal <144057262+Folka95@users.noreply.github.com>
2025-05-08 23:08:46 +0300

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,7 @@
-using System;
-using System.IO;
-using System.Text.Json;
-
-namespace InvestApp.Models
+namespace Invest_Application
 {
     public class User
     {
-        public string ID { get; set; }
         public string Name { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
@@ -16,30 +11,6 @@ namespace InvestApp.Models
             Name = string.Empty;
             Username = string.Empty;
             Password = string.Empty;
-            ID = GenerateID();
-        }
-
-        private static string GenerateID(){
-            string newID = string.Empty;
-            // implement ID generation after adding DB files to iterate through and set the first free ID 
-            return newID;
-        }
-
-        public void Save(string path)
-        {
-            var jsonString = JsonSerializer.Serialize(this);
-            File.WriteAllText(path, jsonString);
-        }
-
-        public static User Load(string path)
-        {
-            if (!File.Exists(path))
-                throw new FileNotFoundException("User data file not found");
-
-            var jsonString = File.ReadAllText(path);
-            var user = JsonSerializer.Deserialize<User>(jsonString);
-            
-            return user ?? throw new Exception("Failed to deserialize user data");
         }
     }
 }
