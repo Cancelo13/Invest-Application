996350dbe1af6c0e30fac9d6bb41c975d6137f96
Folka95 <mohamedkgm95@gmail.com>
2025-05-10 13:39:40 +0300

    update Report generator

--- Models/ReportGenerator.cs
+++ Models/ReportGenerator.cs
@@ -1,10 +1,27 @@
+using ClosedXML.Excel;
+
 namespace Invest_Application
 {
-    public class ReportGenerator
+    public static class ReportGenerator
     {
-        public void GenerateReport(string username)
+        public static void GenerateReport()
         {
+            // Get the path to the user's desktop
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
+            // Define the file name and full path
+            string filePath = Path.Combine(desktopPath, "SampleExcel.xlsx");
+
+            // Create a new workbook and worksheet
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Sheet1");
+                worksheet.Cell("A1").Value = "Hello";
+                worksheet.Cell("B1").Value = "World";
+
+                // Save to the Desktop
+                workbook.SaveAs(filePath);
+            }
         }
     }
 }
